41e95328f446d020c4a1d324e364bb26f07a9385
Juan Camilo Ayala <juanksr0@gmail.com>
2025-06-13 16:50:26 -0500

    Reconocimiento facial y validación de hardware.

--- GalaxyPay/src/components/auth/ByometryAuth.tsx
+++ GalaxyPay/src/components/auth/ByometryAuth.tsx
@@ -1,24 +1,42 @@
-import React from "react";
-import { Alert, Pressable, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Alert, Pressable } from "react-native";
 import ReactNativeBiometrics from "react-native-biometrics";
+import FingerPrint from '../../assets/icons/fingerprint.svg';
 
 export default function BiometryAuth() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasBiometricHardware, setHasBiometricHardware] = useState(false);
+
+  useEffect(() => {
+    const checkBiometricSupport = async () => {
+      const rnBiometrics = new ReactNativeBiometrics();
+      try {
+        const { available } = await rnBiometrics.isSensorAvailable();
+        setHasBiometricHardware(available);
+      } catch (error) {
+        setHasBiometricHardware(false);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    checkBiometricSupport();
+  }, []);
+
   const handleAuth = async () => {
     const rnBiometrics = new ReactNativeBiometrics();
 
-    // Verifica si el dispositivo soporta biometría y si está configurada
-    const { available, biometryType } = await rnBiometrics.isSensorAvailable();
-
-    if (!available) {
-      Alert.alert(
-        "Error",
-        "Tu dispositivo no soporta autenticación biométrica o no está configurada."
-      );
-      return;
-    }
-
     try {
-      // Ejecuta la autenticación biométrica
+      const { available, biometryType } = await rnBiometrics.isSensorAvailable();
+
+      if (!available) {
+        Alert.alert(
+          "Error",
+          "Tu dispositivo no soporta autenticación biométrica o no está configurada."
+        );
+        return;
+      }
+
       const result = await rnBiometrics.simplePrompt({
         promptMessage: "Autenticación - Galaxy Pay",
         fallbackPromptMessage: "Usar contraseña",
@@ -35,8 +53,14 @@ export default function BiometryAuth() {
   };
 
   return (
-    <Pressable style={{ marginTop: 100 }} onPress={handleAuth}>
-      <Text>Iniciar con huella</Text>
-    </Pressable>
+    !isLoading &&
+    hasBiometricHardware && (
+      <Pressable
+        style={({ pressed }) => ({ marginTop: 40, opacity: pressed ? 0.5 : 1 })}
+        onPress={handleAuth}
+      >
+        <FingerPrint color="#E7458F" />
+      </Pressable>
+    )
   );
 }
